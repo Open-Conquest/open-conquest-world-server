2f8f29743173b3be582102e6a4afcfb90ed6aa59
Zach Wild <zacharyjwild@gmail.com>
2020-01-28 12:50:41 -0500

    wip

--- test/integration/repos/ArmyRepository.js
+++ test/integration/repos/ArmyRepository.js
@@ -1,46 +1,25 @@
-const log     = require('../../../src/utils/log');
-const chai    = require('chai');
-const should  = chai.should();
-const expect  = chai.expect;
-const assert  = chai.assert;
+const log = require('../../../src/utils/log');
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
 
 // What we are testing
-const ArmyRepository  = require('../../../src/repos/ArmyRepository');
-const ArmyRepository  = new ArmyRepository();
+const ArmyRepository = require('../../../src/repos/ArmyRepository');
+const armyRepository = new ArmyRepository();
 
 describe('ArmyRepository', function() {
-
   /**
    * This test is meant to make sure that the repo returns the expected armies
    * in the expected format `ArmyEntity`.
    */
   it('should get expected test armies', function() {
-    
-    return armyRepository.getAllArmies().then(res => {
-    
-    });
-    
-    return armyServices.handle(request).then(res => {
-
-      let army_data = res.data;
-      assert(army_data.length > 0);
-      // ensure that armies being returned have all the data expected
-      for (let i = 0; i < army_data.length; i++) {
-        let army = army_data[i];
-        expect(army.army_id).to.be.not.null;
-        expect(army.user_id).to.be.not.null;
-        expect(army.army_units).to.be.not.null;
-        if (army.army_units.length > 0) {
-          for (let j = 0; j < army.army_units.length; j++) {
-            let units = army.army_units[j];
-            expect(units.army_units).to.be.not.null;
-            expect(units.unit_id).to.be.not.null;
-            expect(units.unit_count).to.be.not.null;
-          }
-        }
-      }
-
-    }).catch(err => {
+    return armyRepository.getAllArmies().then((res) => {
+      // make sure that the armies are army entities
+      const armies = res;
+      // make sure that armies is of the correct type
+      expect(armies).to.be.not.null;
+    }).catch((err) => {
       log(err);
       throw err;
     });
