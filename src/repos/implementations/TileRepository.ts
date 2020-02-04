9b2b577c11bd2ced55cd8266b8ea3c34d2acebb4
Zach Wild <zacharyjwild@gmail.com>
2020-02-04 11:50:27 -0500

    Add TileRepository

--- src/repos/implementations/TileRepository.ts
+++ src/repos/implementations/TileRepository.ts
@@ -1,3 +1,6 @@
+import {Tile} from '../../domain/Tile';
+import {models} from '../../models';
+
 /**
  * A Sequelize implementation of the `ITileRepository`
  * @class TileRepository
@@ -16,5 +19,27 @@ export class TileRepository {
    */
   getAllTiles() {
     throw new Error('no implmentation');
+  }
+
+  /**
+   * Return a tile for row, col.
+   *
+   * @param {number} row
+   * @param {number} col
+   * @return {Promise<Tile>}
+   * @memberof TileRepository
+   */
+  getTile(row: number, col: number) {
+    return new Promise(function(resolve, reject) {
+      models.tile.findOne({
+        where: {tile_row: row, tile_col: col},
+      })
+          .then((tile) => {
+            resolve(tile);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+    });
   }
 }
