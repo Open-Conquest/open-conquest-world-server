0123ff2d98096d087619cfd376a0c3ef5954a643
Zach Wild <zacharyjwild@gmail.com>
2020-02-06 13:42:23 -0500

    Add some comments

--- test/integration/repos/implementations/UserRepository.ts
+++ test/integration/repos/implementations/UserRepository.ts
@@ -6,18 +6,19 @@ import {User} from '../../../../src/domain/User';
 import {models} from '../../../../src/models';
 
 const assert = chai.assert;
-
 const userRepository = new UserRepository();
 
-const connection = models.sequelize;
-
 describe('UserRepository', function() {
+  // Start transaction before each test
+  const connection = models.sequelize;
   beforeEach(() => {
     return connection.query('START TRANSACTION');
   });
+  // Rollback any database changes made during a test
   afterEach(() => {
     return connection.query('ROLLBACK');
   });
+
   it('createNewUser should create a new user', async function() {
     // create a new user that would come in through a request
     const user = new User(null, 'test_username');
@@ -30,6 +31,7 @@ describe('UserRepository', function() {
           throw err;
         });
   });
+
   it('createNewUser should fail with a duplicate username', async function() {
     // create a new user that would come in through a request
     const user = new User(null, 'test_username');
