f396a9c3b99a2eb90e86209f61290e122b8fc1d9
Zach Wild <zacharyjwild@gmail.com>
2020-02-06 12:36:24 -0500

    Add username to domain User

--- src/domain/User.ts
+++ src/domain/User.ts
@@ -8,13 +8,27 @@ import {Entity, EntityId} from './Entity';
  * @extends {Entity}
  */
 export class User extends Entity {
+  private username: string;
+
   /**
    * Creates an instance of User.
    *
    * @param {EntityId} id
+   * @param {string} username
    * @memberof User
    */
-  constructor(id: EntityId) {
+  constructor(id: EntityId, username: string) {
     super(id);
+    this.username = username;
+  }
+
+  /**
+   * Returns this user's username.
+   *
+   * @return {string}
+   * @memberof User
+   */
+  getUsername(): string {
+    return this.username;
   }
 }
