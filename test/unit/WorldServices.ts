5d50c5d5b2a32488629e098e5f0b8db1dfa02f93
Zach Wild <zacharyjwild@gmail.com>
2020-02-06 19:29:22 -0500

    Add unit tests for WorldServices

---
+++ test/unit/WorldServices.ts
@@ -0,0 +1,70 @@
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+import {WorldServices} from "../../src/WorldServices"
+import {BaseServices} from "../../src/services/BaseServices";
+import {ArmyServices} from "../../src/services/ArmyServices";
+import {UserServices} from "../../src/services/UserServices";
+import {Request} from "../../src/Request";
+import {Response} from "../../src/Response";
+import {ServiceNames} from "../../src/services/ServiceNames";
+import {ServiceOperations} from "../../src/services/ServiceOperations";
+import {log} from '../../src/utils/log';
+
+const assert = chai.assert;
+
+/**
+ * Test service for testing WorldServices dispatchRequest method.
+ */
+class TestService extends BaseServices {
+  // eslint-disable-next-line require-jsdoc
+  constructor() {
+    super();
+    this.serviceName = ServiceNames.Test;
+  }
+  // eslint-disable-next-line require-jsdoc
+  handle(request: Request): Promise<Response> {
+    return new Promise( function(resolve, reject) {
+      resolve(new Response(ServiceNames.Test, ServiceOperations.Test, true));
+    });
+  }
+}
+
+/**
+ * Extending ServiceNames and ServiceOperations enums for tests.
+ */
+declare module '../../src/services/ServiceNames' {
+  export enum ServiceNames {
+   Test = 'Test'
+  }
+}
+declare module '../../src/services/ServiceOperations' {
+  export enum ServiceOperations {
+   Test = 'Test'
+  }
+}
+
+describe('WorldService', function() {
+  it('should dispatch the request to the correct service', async function() {
+    // create new world services
+    const worldServices = new WorldServices();
+    // register services with world services
+    const testService = new TestService();
+    const armyService = new ArmyServices();
+    const userService = new UserServices();
+    worldServices.registerService(armyService);
+    worldServices.registerService(testService);
+    worldServices.registerService(userService);
+    // make a request that should be dispatched to test services
+    const request = new Request(ServiceNames.Test, ServiceOperations.Test, {});
+    return worldServices.dispatchRequest(request)
+        .then((res) => {
+          assert(res.getService() === ServiceNames.Army);
+          assert(res.getOperation() === ServiceOperations.Test);
+          assert(res.getData() === true);
+        })
+        .catch((err) => {
+          log(err);
+          throw err;
+        });
+  });
+});
