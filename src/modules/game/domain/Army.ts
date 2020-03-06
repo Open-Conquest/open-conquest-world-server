99d1b374dd50f837c784cae954fbe65da69b8d34
Zach Wild <zacharyjwild@gmail.com>
2020-03-05 23:40:38 -0500

    Add units to Army entity

--- src/modules/game/domain/Army.ts
+++ src/modules/game/domain/Army.ts
@@ -1,6 +1,7 @@
 /* eslint-disable require-jsdoc */
 import {Entity} from '../../../shared/domain/Entity';
 import {EntityID} from '../../../shared/domain/EntityID';
+import {ArmyUnits} from './ArmyUnits';
 
 /**
  * Domain entity representation of a army.
@@ -11,17 +12,20 @@ import {EntityID} from '../../../shared/domain/EntityID';
  */
 export class Army extends Entity {
   private playerID: EntityID;
+  private units: Array<ArmyUnits>;
 
   /**
    * Create an instance of a army entity.
    *
    * @param {EntityID} id
    * @param {PlayerID} playerID
+   * @param {Array<ArmyUnits>} units
    * @memberof Army
    */
-  constructor(id: EntityID, playerID: EntityID) {
+  constructor(id: EntityID, playerID: EntityID, units: Array<ArmyUnits>) {
     super(id);
-    this.$playerID = playerID;
+    this.playerID = playerID;
+    this.units = units;
   }
 
   public get $playerID(): EntityID {
@@ -30,5 +34,13 @@ export class Army extends Entity {
 
   public set $playerID(value: EntityID) {
     this.playerID = value;
+  }
+
+  public get $units(): Array<ArmyUnits> {
+    return this.units;
+  }
+
+  public set $units(value: Array<ArmyUnits>) {
+    this.units = value;
   }
 }
