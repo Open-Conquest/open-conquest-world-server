b35bc96f8596a8e860a8e530fa708cd44643f293
Zach Wild <zacharyjwild@gmail.com>
2020-03-05 23:41:28 -0500

    Add createWizard to unit factory

--- src/modules/game/factories/UnitFactory.ts
+++ src/modules/game/factories/UnitFactory.ts
@@ -1,5 +1,6 @@
+/* eslint-disable valid-jsdoc */
 /* eslint-disable require-jsdoc */
-import {Unit, UnitName} from '../domain/Unit';
+import {Unit, UnitName, UnitType, UnitAttack, UnitDefense, UnitGoldCost} from '../domain/Unit';
 import {EntityID} from '../../../shared/domain/EntityID';
 
 /**
@@ -16,6 +17,8 @@ export class UnitFactory {
    */
   constructor() {}
 
+
+  /* Create a new Unit with all of its properties */
   createUnit(type: number, name: string, attack: number, defense: number, goldCost: number): Unit {
     return new Unit(
         type,
@@ -23,6 +26,17 @@ export class UnitFactory {
         attack,
         defense,
         goldCost,
+    );
+  }
+
+  /* Create a wizard Unit */
+  createWizard(): Unit {
+    return new Unit(
+        UnitType.Wizard,
+        UnitName.Wizard,
+        UnitAttack.Wizard,
+        UnitDefense.Wizard,
+        UnitGoldCost.Wizard,
     );
   }
 }
