788f45e2a62a0501839c3271ab45268fe8d43d73
Zach Wild <zacharyjwild@gmail.com>
2020-03-05 23:42:10 -0500

    Add errors for ArmyUnitsRepo

--- src/modules/game/repos/ArmyUnitsRepositoryErrors.ts
+++ src/modules/game/repos/ArmyUnitsRepositoryErrors.ts
@@ -1,3 +1,5 @@
 export enum ArmyUnitsRepositoryErrors {
-  NonexistentArmy = 'tried to units for a nonexistent army'
+  NonexistentArmy = 'tried to units for a nonexistent army',
+  NonexistentPlayer = 'tried to units for a nonexistent player',
+  NonexistentUnit = 'tried to create army units with nonexistent unit'
 }
