706ae618b5a3211d98e9625aed485afcb32bead4
Zach Wild <zacharyjwild@gmail.com>
2020-03-05 20:00:06 -0500

    Add ArmyUnits domain entity

--- src/modules/game/domain/ArmyUnits.ts
+++ src/modules/game/domain/ArmyUnits.ts
@@ -1,6 +1,7 @@
 /* eslint-disable require-jsdoc */
 import {Entity} from '../../../shared/domain/Entity';
 import {EntityID} from '../../../shared/domain/EntityID';
+import {Unit} from './Unit';
 
 /**
  * Domain entity representation of a armyunits.
@@ -11,17 +12,23 @@ import {EntityID} from '../../../shared/domain/EntityID';
  */
 export class ArmyUnits extends Entity {
   private armyID: EntityID;
+  private count: number;
+  private unit: Unit;
 
   /**
    * Create an instance of a armyunits entity.
    *
    * @param {EntityID} id
    * @param {PlayerID} armyID
+   * @param {number} count
+   * @param {Unit} unit
    * @memberof ArmyUnits
    */
-  constructor(id: EntityID, armyID: EntityID) {
+  constructor(id: EntityID, armyID: EntityID, count: number, unit: Unit) {
     super(id);
-    this.$armyID = armyID;
+    this.armyID = armyID;
+    this.count = count;
+    this.unit = unit;
   }
 
   public get $armyID(): EntityID {
@@ -30,5 +37,21 @@ export class ArmyUnits extends Entity {
 
   public set $armyID(value: EntityID) {
     this.armyID = value;
+  }
+
+  public get $count(): number {
+    return this.count;
+  }
+
+  public get $unit(): Unit {
+    return this.unit;
+  }
+
+  public set $count(value: number) {
+    this.count = value;
+  }
+
+  public set $unit(value: Unit) {
+    this.unit = value;
   }
 }
