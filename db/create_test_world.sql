5e463c8890d4492d0309bbc3d97953d681b3cce1
Zach Wild <zacharyjwild@gmail.com>
2020-03-12 17:22:11 -0400

    (#23) Bug fixes for create player
    * Update database creation scripts
    * Fix error not getting token value properly from message when validating jwt

---
+++ db/create_test_world.sql
@@ -0,0 +1,51 @@
+-- A script to create the initial state of a world for alpha testing
+-- What it does:
+-- 		Creates a 10x10 map called "America"
+-- 		Makes every tile in "America" grass
+-- 		Creates the basic unit types of Wizard & Bear
+
+USE master;
+
+# CREATE MAP AMERICA
+SET @map_id = 0;
+SET @num_rows = 10;
+SET @num_cols = 10;
+INSERT INTO map (map_name, max_rows, max_cols) VALUES ('America', @num_rows, @num_cols);
+SELECT map_id FROM map INTO @map_id;
+
+# CREATE TILES FOR AMERICA
+SET @tile_type = 0;
+DROP PROCEDURE IF EXISTS CreateTiles;
+DELIMITER $$
+CREATE PROCEDURE CreateTiles()
+BEGIN
+    DECLARE row_num INT;
+    DECLARE col_num INT;
+    SET row_num = 0;
+    SET col_num = 0;
+    row_loop:  LOOP
+        IF row_num >= @num_rows THEN
+			LEAVE row_loop;
+		END IF;
+        SET col_num = 0;
+		col_loop: LOOP
+			IF col_num >= @num_cols THEN
+				LEAVE col_loop;
+			END IF;
+            SET @tile_type = ROUND((RAND() * (25-0))+0);
+            SET @tile_type = 0;
+			INSERT INTO tile (map_id, tile_type, tile_row, tile_col) VALUES (@map_id, @tile_type, row_num, col_num);
+			SET col_num = col_num + 1;
+		END LOOP;
+        SELECT row_num;
+		SET row_num = row_num + 1;
+	END LOOP;
+END$$
+DELIMITER ;
+Call CreateTiles();
+
+# CREAT THE BASIC UNIT TYPES WIZARD AND BEAR
+SET @wizard_unit_id = 0;
+SET @bear_unit_id = 1;
+INSERT INTO unit (unit_id, attack, defense, name, level, gold_cost) VALUES (@wizard_unit_id, 100, 50, "Wizard", 1, 100);
+INSERT INTO unit (unit_id, attack, defense, name, level, gold_cost) VALUES (@bear_unit_id, 30, 200, "Bear", 1, 150);		
