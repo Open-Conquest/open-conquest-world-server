f71b4a25e6cb7610321f8d0c1757818f0919171e
Zach Wild <zacharyjwild@gmail.com>
2020-07-03 23:53:34 -0400

    Start playing with decorators

--- test/integration/modules/user/repos/implementations/UserRepository.ts
+++ test/integration/modules/user/repos/implementations/UserRepository.ts
@@ -27,12 +27,13 @@ const expect = chai.expect;
 describe('UserRepository:createUser', function() {
   // Start transaction before each test
   const connection = models.sequelize;
-  beforeEach(() => {
-    return connection.query('START TRANSACTION');
+  let t = null;
+  beforeEach(async () => {
+    t = await connection.transaction();
   });
   // Rollback any database changes made during a test
-  afterEach(() => {
-    return connection.query('ROLLBACK');
+  afterEach(async () => {
+    await t.rollback();
   });
 
   it('Should create a new user', async function() {
@@ -45,8 +46,7 @@ describe('UserRepository:createUser', function() {
         password,
     );
 
-    // create user in database
-    const createdUser = await userRepository.createUser(user);
+    const createdUser = await userRepository.testcreateUser(user, t);
 
     // assert user was created with expected values
     assert(createdUser.$username.$value === username);
@@ -58,18 +58,62 @@ describe('UserRepository:createUser', function() {
     const password = 'hashed_password32f14edfq';
     const user = userFactory.createUserWithHashedPassword(null, username, password);
 
-    return userRepository.createUser(user)
-        .then((newUser) => {
-          return userRepository.createUser(user);
-        })
-        .then((newUser) => {
-          assert.fail('User with duplicate username shouldn\'t have been created');
-        })
-        .catch((err) => {
-          expect(err.message).to.equal(UserRepositoryErrors.DuplicateUsername);
-        });
+    const firstUser = await userRepository.createUser(user);
+
+    try {
+      const secondUser = await userRepository.createUser(user);
+      assert.fail('User with duplicate username shouldn\'t have been created');
+    } catch (err) {
+      expect(err.message).to.equal(UserRepositoryErrors.DuplicateUsername);
+    }
   });
 });
+
+// eslint-disable-next-line require-jsdoc
+export function logMethod(
+    target: Object,
+    propertyName: string,
+    propertyDesciptor: PropertyDescriptor): PropertyDescriptor {
+  // target === Employee.prototype
+  // propertyName === "greet"
+  // propertyDesciptor === Object.getOwnPropertyDescriptor(Employee.prototype, "greet")
+  const method = propertyDesciptor.value;
+
+  propertyDesciptor.value = function(...args: any[]) {
+    // convert list of greet arguments to string
+    const params = args.map((a) => JSON.stringify(a)).join();
+
+    // invoke greet() and get its return value
+    const result = method.apply(this, args);
+
+    // convert result to string
+    const r = JSON.stringify(result);
+
+    // display in console the function call details
+    console.log(`Call: ${propertyName}(${params}) => ${r}`);
+
+    // return the result of invoking the method
+    return result;
+  };
+  return propertyDesciptor;
+};
+
+class Employee {
+  constructor(
+      private firstName: string,
+      private lastName: string,
+  ) {
+  }
+
+  @logMethod
+  greet(message: string): string {
+    return `${this.firstName} ${this.lastName} says: ${message}`;
+  }
+}
+
+const emp = new Employee('Mohan Ram', 'Ratnakumar');
+emp.greet('hello');
+
 
 describe('UserRepository:getPasswordWithUsername', function() {
   // Start transaction before each test
@@ -87,6 +131,7 @@ describe('UserRepository:getPasswordWithUsername', function() {
     const username = 'test_username';
     const password = 'hashed_password32f14edfq';
     const user = userFactory.createUserWithHashedPassword(null, username, password);
+
     await userRepository.createUser(user);
 
     // try to get user
